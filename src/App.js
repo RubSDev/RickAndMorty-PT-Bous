5741a1912d5de2350108460516b6785f7045a809
RubS <rubasomuano.g2@gmail.com>
2022-11-11 22:01:40 -0600

    first commit Feature: Fetching, Cards, Pagination, Search y Filters

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import Header from "./components/Header/Header";
+import Filters from "./components/Filters/Filters";
+import Cards from "./components/Cards/Cards";
+import Pagination from "./components/Pagination/Pagination";
+import Search from "./components/Search/Search";
 
 function App() {
+  let [pageNumber, setPageNumber] = useState(1);
+  let [search, setSearch] = useState("");
+  let [status, setStatus] = useState("")
+  let [gender, setGender] = useState("")
+  let [species, setSpecies] = useState("")
+  let [fetchedData, updateFetchedData] = useState([]);
+  let { info, results } = fetchedData;
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}&status=${status}&gender=${gender}&species=${species}`;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      updateFetchedData(data);
+    })();
+  }, [api]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="d-flex justify-content-center gap-5 align-items-center">
+        <h1 className="text-start ubuntu my-4 ">Rick &amp; Morty </h1>
+        <img src="Bouslogo-color.svg" alt="LogoBous"></img>
+      </div>
+      <Search setPageNumber={setPageNumber} setSearch={setSearch} />
+      <div className="container">
+        <div className="row">
+         
+            <Filters setStatus={setStatus} setPageNumber={setPageNumber} setGender={setGender} setSpecies={setSpecies}/>
+        
+          <div className="col-8">
+            <div className="row">
+              <Cards results={results} />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Pagination info={info} pageNumber={pageNumber} setPageNumber={setPageNumber} />
     </div>
   );
 }
---
+++ src/components/Filters/Filters.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Gender from "../Filters/Category/Gender";
+import Species from "../Filters/Category/Species";
+import Status from "../Filters/Category/Status";
+
+const Filters = ({setGender, setStatus, setPageNumber, setSpecies}) => {
+
+
+  let clear = ()=>{
+    setGender("") 
+    setStatus("") 
+    setPageNumber("") 
+    setSpecies("")
+    window.location.reload(false)
+  }
+
+  return (
+    <div className="col-3">
+      <div className="text-center fw-bold fs-4 mb-2">Filtro</div>
+      <div
+      onClick={clear}
+        style={{ cursor: "pointer" }}
+        className="text-center text-primary text-decoration-underline mb-4"
+      >
+        Borrar filtros
+      </div>
+
+      <div className="accordion" id="accordionExample">
+       <Status setPageNumber={setPageNumber} setStatus={setStatus}/>       
+       <Species setPageNumber={setPageNumber} setSpecies={setSpecies}/>
+       <Gender setPageNumber={setPageNumber} setGender={setGender}/>
+      </div>
+
+
+    </div>
+  );
+};
+
+export default Filters;
