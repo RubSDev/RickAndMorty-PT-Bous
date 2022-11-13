72268306871cc748ef1eb63f3e05d708f6416a49
RubS <rubasomuano.g2@gmail.com>
2022-11-12 18:07:39 -0600

    Fix inputGroup

--- src/components/Filters/Category/InputGroup.js
+++ src/components/Filters/Category/InputGroup.js
@@ -1,20 +1,21 @@
 import React from "react";
 
-const InputGroup = ({total, name, setID}) => {
- 
+const InputGroup = ({ name, setID, total }) => {
   return (
     <div className="input-group mb-3">
-      <select 
-      onChange={e=>setID(e.target.value)}
-      className="custom-select" id={name}>
-        <option value="1" selected>Elige...</option>
-
-        {[...Array(total).keys()].map(x=>{
-          return(
-            <option value={x+1}>{name} - {x+1}</option>
-          )
+      <select
+        onChange={(e) => setID(e.target.value)}
+        className="form-select"
+        id={name}
+      >
+        <option value="1">Choose...</option>
+        {[...Array(total).keys()].map((x, index) => {
+          return (
+            <option value={x + 1}>
+              {name} - {x + 1}
+            </option>
+          );
         })}
-     
       </select>
     </div>
   );
